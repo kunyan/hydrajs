9643f0ba02d1d30b0137fc04a47dcba74073c65e
Samuel Mendenhall <Samuel.Mendenhall@gmail.com>
2017-01-12 13:02:57 -0500

    Adding sso, removing prepublish

--- src/models/case.ts
+++ src/models/case.ts
@@ -46,16 +46,21 @@ export interface ICase_Comment__c {
     Last_Modified_By_Contact__r: IContact;
 }
 
-// Not sure Typescript can elegantly handle nested keyof references, hardcode for now
+// Not sure Typescript can elegantly handle nested keyof references, hard code for now.  While verbose, the primary
+// benefit of defining this is to allow for typed field integrity when performaing API calls against Hydra
 interface ICase_Comment__c_nested {
     'Created_By_User__r.Id': string;
     'Created_By_User__r.Full_Name__c': string;
+    'Created_By_User__r.SSO_Username__c': string;
     'Last_Modified_By_User__r.Id': string;
     'Last_Modified_By_User__r.Full_Name__c': string;
+    'Last_Modified_By_User__r.SSO_Username__c': string;
     'Created_By_Contact__r.Id': string;
     'Created_By_Contact__r.Full_Name__c': string;
+    'Created_By_Contact__r.SSO_Username__c': string;
     'Last_Modified_By_Contact__r.Id': string;
     'Last_Modified_By_Contact__r.Full_Name__c': string;
+    'Last_Modified_By_Contact__r.SSO_Username__c': string;
 }
 
 export type ICase_Comment__c_fields = Array<keyof ICase_Comment__c | keyof ICase_Comment__c_nested>;
