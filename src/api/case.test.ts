edcd492c77ac5b54a387bdd47f85505950837d0b
Kun Yan <kyan@redhat.com>
2017-02-09 15:23:23 +0800

    Use AVA replace mocha

--- src/api/case.test.ts
+++ src/api/case.test.ts
@@ -1,69 +1,63 @@
-import 'mocha';
-import chai = require('chai');
-const expect = chai.expect;
+import test from 'ava';
 import * as caseApi from './case';
-import {ICase_fields} from '../models/case';
+import { ICase_fields } from '../models/case';
 
-describe('Case Tests', () => {
-    describe('/case/{caseNumber}', () => {
-        it('should return specified fields for case 00023622', () => {
-            const fields: ICase_fields = [
-                'Id',
-                'CaseNumber',
-                'LastModifiedBy.Id',
-                'LastModifiedBy.Full_Name__c',
-                'LastModifiedBy.SSO_Username__c',
-                'LastModifiedBy.Email',
-                'LastModifiedBy.Phone',
-                'LastModifiedBy.TimeZoneSidKey',
+test('should return specified fields for case 00023622', async t => {
+    const fields: ICase_fields = [
+        'Id',
+        'CaseNumber',
+        'LastModifiedBy.Id',
+        'LastModifiedBy.Full_Name__c',
+        'LastModifiedBy.SSO_Username__c',
+        'LastModifiedBy.Email',
+        'LastModifiedBy.Phone',
+        'LastModifiedBy.TimeZoneSidKey',
 
-                'Entitlement.Id',
-                'Entitlement.Name',
+        'Entitlement.Id',
+        'Entitlement.Name',
 
-                'Created_By_User__r.Id',
-                'Created_By_User__r.Full_Name__c',
-                'Created_By_User__r.SSO_Username__c',
-                'Created_By_User__r.Email',
-                'Created_By_User__r.Phone',
-                'Created_By_User__r.TimeZoneSidKey',
+        'Created_By_User__r.Id',
+        'Created_By_User__r.Full_Name__c',
+        'Created_By_User__r.SSO_Username__c',
+        'Created_By_User__r.Email',
+        'Created_By_User__r.Phone',
+        'Created_By_User__r.TimeZoneSidKey',
 
-                'Created_By_Contact__r.Id',
-                'Created_By_Contact__r.Full_Name__c',
-                'Created_By_Contact__r.SSO_Username__c',
-                'Created_By_Contact__r.Email',
-                'Created_By_Contact__r.Phone',
-                'Created_By_Contact__r.Timezone__c',
+        'Created_By_Contact__r.Id',
+        'Created_By_Contact__r.Full_Name__c',
+        'Created_By_Contact__r.SSO_Username__c',
+        'Created_By_Contact__r.Email',
+        'Created_By_Contact__r.Phone',
+        'Created_By_Contact__r.Timezone__c',
 
-                'Case_Owner__r.Id',
-                'Case_Owner__r.Full_Name__c',
-                'Case_Owner__r.Full_Name__c',
-                'Case_Owner__r.SSO_Username__c',
-                'Case_Owner__r.Email',
-                'Case_Owner__r.Phone',
-                'Case_Owner__r.TimeZoneSidKey',
+        'Case_Owner__r.Id',
+        'Case_Owner__r.Full_Name__c',
+        'Case_Owner__r.Full_Name__c',
+        'Case_Owner__r.SSO_Username__c',
+        'Case_Owner__r.Email',
+        'Case_Owner__r.Phone',
+        'Case_Owner__r.TimeZoneSidKey',
 
-                'Bug__r.Id',
-                'Bug__r.Name',
+        'Bug__r.Id',
+        'Bug__r.Name',
 
-                'ExternalLockBy__r.Id',
-                'ExternalLockBy__r.Full_Name__c',
-                'ExternalLockBy__r.SSO_Username__c',
-                'ExternalLockBy__r.Email',
-                'ExternalLockBy__r.Phone',
-                'ExternalLockBy__r.TimeZoneSidKey',
+        'ExternalLockBy__r.Id',
+        'ExternalLockBy__r.Full_Name__c',
+        'ExternalLockBy__r.SSO_Username__c',
+        'ExternalLockBy__r.Email',
+        'ExternalLockBy__r.Phone',
+        'ExternalLockBy__r.TimeZoneSidKey',
 
-                'Account.Id',
-                'Account.AccountNumber',
-                'Account.Name',
-                'Account.Super_Region__c',
-                'Account.Is_Active__c',
-            ];
-            return caseApi.getCase('00023622', fields).then((kase) => {
-                expect(kase).to.exist;
-                expect(kase.Id).to.exist;
-                expect(kase.CaseNumber).to.eql('00023622');
-                expect(kase.Account.Id).to.eql('001A000000K8A0dIAF');
-            });
-        });
-    });
+        'Account.Id',
+        'Account.AccountNumber',
+        'Account.Name',
+        'Account.Super_Region__c',
+        'Account.Is_Active__c',
+    ];
+
+    const kase = await caseApi.getCase('00023622', fields);
+    t.truthy(kase);
+    t.truthy(kase.Id);
+    t.is(kase.CaseNumber, '00023622');
+    t.is(kase.Account.Id, '001A000000K8A0dIAF');
 });
--- src/api/comment.test.ts
+++ src/api/comment.test.ts
@@ -1,61 +1,51 @@
-import 'mocha';
-import chai = require('chai');
-const expect = chai.expect;
+import test from 'ava';
 import * as caseApi from './comment';
 import {ICase_Comment__c_fields, IAPIComment} from '../models/comment';
 
-describe('Comment Tests', () => {
-    describe('/case/{caseNumber}/comments', () => {
-        it('should return specified fields for case 00023622', () => {
-            const fields: ICase_Comment__c_fields = [
-                'Id',
-                'CaseNumber__c',
-                'Is_Public__c',
-                'Created_By_User__r.Id',
-                'Created_By_User__r.Full_Name__c'
-            ];
-            return caseApi.getComments('00023622', fields).then((comments) => {
-                expect(comments.length).to.be.above(10);
-                expect(comments[0].Id).to.exist;
-                expect(comments[0].Is_Public__c).to.exist;
-                // expect(comments[0].Created_By_User__r.Id).to.exist;
-                // expect(comments[0].Created_By_User__r.Full_Name__c).to.exist;
-            });
-        });
-        it('should create a new private comment on 00023622 and edit it', () => {
-            const apiComment: IAPIComment = {
-                caseNumber: '00023622',
-                text: 'Test private comment',
-                isPublic: false,
-                doNotChangeSBT: false
-            };
-            return caseApi.upsertComment(apiComment).then((response: IAPIComment) => {
-                expect(response.id).to.exist;
-                expect(response.isPublic).to.eql(false);
-                expect(response.returnCode).to.eql(200);
-                expect(response.text).to.eql('Test private comment');
+test('should return specified fields for case 00023622', async t => {
+    const fields: ICase_Comment__c_fields = [
+        'Id',
+        'CaseNumber__c',
+        'Is_Public__c',
+        'Created_By_User__r.Id',
+        'Created_By_User__r.Full_Name__c'
+    ];
+    const comments = await caseApi.getComments('00023622', fields);
+    t.truthy(comments);
+    t.true(comments.length > 10);
+    t.truthy(comments[0].Id);
+    t.truthy(comments[0].Is_Public__c);
+});
 
-                response.text = 'Test private comment updated';
+test('should create a new private comment on 00023622 and edit it', async t => {
+    const apiComment: IAPIComment = {
+        caseNumber: '00023622',
+        text: 'Test private comment',
+        isPublic: false,
+        doNotChangeSBT: false
+    };
+    const comment: IAPIComment = await caseApi.upsertComment(apiComment);
+    t.truthy(comment.id);
+    t.false(comment.isPublic);
+    t.is(comment.returnCode, 200);
+    t.is(comment.text, 'Test private comment');
 
-                return caseApi.upsertComment(response).then((response: IAPIComment) => {
-                    expect(response.returnCode).to.eql(200);
-                    expect(response.text).to.eql('Test private comment updated');
-                });
-            });
-        });
-        it('should create a new public comment on 00023622', () => {
-            const apiComment: IAPIComment = {
-                caseNumber: '00023622',
-                text: 'Test public comment',
-                isPublic: false,
-                doNotChangeSBT: false
-            };
-            return caseApi.upsertComment(apiComment).then((response: IAPIComment) => {
-                expect(response.id).to.exist;
-                expect(response.isPublic).to.eql(false);
-                expect(response.returnCode).to.eql(200);
-                expect(response.text).to.eql('Test public comment');
-            });
-        });
-    });
+    comment.text = 'Test private comment updated';
+    const updatedComment = await caseApi.upsertComment(comment);
+    t.is(updatedComment.returnCode, 200);
+    t.is(updatedComment.text, 'Test private comment updated');
+});
+
+test('should create a new public comment on 00023622', async t => {
+    const apiComment: IAPIComment = {
+        caseNumber: '00023622',
+        text: 'Test public comment',
+        isPublic: true,
+        doNotChangeSBT: false
+    };
+    const comment: IAPIComment = await caseApi.upsertComment(apiComment);
+    t.truthy(comment.id);
+    t.true(comment.isPublic);
+    t.is(comment.returnCode, 200);
+    t.is(comment.text, 'Test public comment');
 });
