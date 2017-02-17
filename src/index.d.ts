2825494d0d8b17bc106ae36c7541109e58d8d52c
Vikas Rathee <vrathee@dhcp193-82.pnq.redhat.com>
2017-02-17 12:13:01 +0530

    Intermediate commit

--- src/index.d.ts
+++ src/index.d.ts
@@ -7,7 +7,7 @@ declare namespace hydrajs {
         export function getComments(caseNumber: string, fields?: ICase_Comment__c_fields): Promise<Array<ICase_Comment__c>>;
         export function upsertComment(apiComment: IAPIComment): Promise<IAPIComment>;
         export function getCase(caseId: string, fields?: ICase_fields): Promise<ICase>;
-    },
+    }
     namespace insights {
         export function runInsights(caseNumber: string, attachmentId: string): Promise<InsightsResult>;
         export function getInsightsRules(ruleIds?: RuleIds): Promise<Array<InsightsRule>>;
