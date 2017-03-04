1f39afbb1137d31d80909d94385d3973c23d7a55
kgandhi <kgandhi@redhat.com>
2017-03-04 18:56:13 +0530

    template related rest endpoint calls

---
+++ src/models/skedge/templateMetadata.ts
@@ -0,0 +1,10 @@
+export interface ITemplateMetadata {
+    templateId?: number;
+    templateCustom?: boolean;
+    woFirst?: number;
+    woSecond?: number;
+    createdBy?: string;
+    createdAt?: number;
+    updateBy?: string;
+    updateAt?: number;
+}
