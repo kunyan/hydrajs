db928fc17a6694cbc4cc3ff2c2c5729220a699cb
Samuel Mendenhall <Samuel.Mendenhall@gmail.com>
2017-06-29 07:55:17 -0400

    Adding isFTSHandoverReady

--- src/models/case.ts
+++ src/models/case.ts
@@ -63,6 +63,7 @@ export interface IAPICase {
     internalstatus?: string;
     isCertification?: boolean;
     isClosed?: boolean;
+    isFTSHandoverReady?: boolean;
     isPoc?: boolean;
     isStrategicAccount?: boolean;
     issue?: string;
